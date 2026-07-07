bd8a3f70d6df210d8e2830ab5788849e1bccd786
thanh <thanhhero765@gmail.com>
2026-07-08 00:19:37 +0700

    Fix LibRaw build errors - remove duplicate sources and use static linking

--- WindowsApp.Core/libraw/src/libraw_c_api.cpp
+++ WindowsApp.Core/libraw/src/libraw_c_api.cpp
@@ -271,7 +271,8 @@ extern "C"
 
   void libraw_dcraw_clear_mem(libraw_processed_image_t *p)
   {
-    LibRaw::dcraw_clear_mem(p);
+    if (p)
+      ::free(p);
   }
 
   int libraw_raw2image(libraw_data_t *lr)
